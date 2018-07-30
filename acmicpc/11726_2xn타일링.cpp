f9ca2913cd79de727c56e32514b941f2f82bb8ac
skrudtn <skrudtn703@naver.com>
2018-07-30 19:51:30 +0900

    Add source code

---
+++ acmicpc/11726_2xn타일링.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int dp[1001];
+int main(void){
+	int n;
+	scanf("%d",&n);
+	dp[1]=1;
+	dp[2]=2;
+	for(int i=3; i<=n; ++i){
+		dp[i]=(dp[i-1]+dp[i-2])%10007;
+	}
+	printf("%d",dp[n]);
+	return 0;
+}
