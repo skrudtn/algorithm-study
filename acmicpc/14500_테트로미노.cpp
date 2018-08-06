1ddb842c0c60be7b6e3afe914056e9c217282c74
skrudtn <skrudtn703@naver.com>
2018-08-06 18:34:46 +0900

    Add source code with dfs

--- acmicpc/14500_테트로미노.cpp
+++ acmicpc/14500_테트로미노.cpp
@@ -2,184 +2,54 @@
 
 using namespace std;
 
-int n,m,map[501][501];
-
-int block[7][4][4][4]={
-{
-	{
-	{1,1,1,1},
-	{0,0,0,0},
-	{0,0,0,0},
-	{0,0,0,0}
-	},
-	{
-	{1,0,0,0},
-	{1,0,0,0},
-	{1,0,0,0},
-	{1,0,0,0}
-	},
-},
-{
-	{
-	{1,1,0,0},
-	{1,1,0,0},
-	{0,0,0,0},
-	{0,0,0,0}
-	},
-},
-{
-	{
-	{1,0,0,0},
-	{1,0,0,0},
-	{1,1,0,0},
-	{0,0,0,0}
-	},
-	{
-	{1,1,1,0},
-	{1,0,0,0},
-	{0,0,0,0},
-	{0,0,0,0}
-	},
-	{
-	{1,1,0,0},
-	{0,1,0,0},
-	{0,1,0,0},
-	{0,0,0,0}
-	},
-	{
-	{0,0,1,0},
-	{1,1,1,0},
-	{0,0,0,0},
-	{0,0,0,0}
-	},
-},
-{
-	{
-	{0,1,0,0},
-	{0,1,0,0},
-	{1,1,0,0},
-	{0,0,0,0}
-	},
-	{
-	{1,0,0,0},
-	{1,1,1,0},
-	{0,0,0,0},
-	{0,0,0,0}
-	},
-	{
-	{1,1,0,0},
-	{1,0,0,0},
-	{1,0,0,0},
-	{0,0,0,0}
-	},
-	{
-	{1,1,1,0},
-	{0,0,1,0},
-	{0,0,0,0},
-	{0,0,0,0}
-	},
-},
-{
-	{
-	{1,0,0,0},
-	{1,1,0,0},
-	{0,1,0,0},
-	{0,0,0,0}
-	},
-	{
-	{0,1,1,0},
-	{1,1,0,0},
-	{0,0,0,0},
-	{0,0,0,0}
-	},
-},
-{
-	{
-	{0,1,0,0},
-	{1,1,0,0},
-	{1,0,0,0},
-	{0,0,0,0}
-	},
-	{
-	{1,1,0,0},
-	{0,1,1,0},
-	{0,0,0,0},
-	{0,0,0,0}
-	},
-},
-{
-	{
-	{1,1,1,0},
-	{0,1,0,0},
-	{0,0,0,0},
-	{0,0,0,0}
-	},
-	{
-	{0,1,0,0},
-	{1,1,0,0},
-	{0,1,0,0},
-	{0,0,0,0}
-	},
-	{
-	{0,1,0,0},
-	{1,1,1,0},
-	{0,0,0,0},
-	{0,0,0,0}
-	},
-	{
-	{1,0,0,0},
-	{1,1,0,0},
-	{1,0,0,0},
-	{0,0,0,0}
-	},
-}
-};
+int ans,n,m,map[501][501],visit[501][501];
+int dx[4]={1,0,-1,0};
+int dy[4]={0,1,0,-1};
 
 int _max(int a,int b){return a>b?a:b;}
+int _min(int a,int b){return a<b?a:b;}
 
 int isOnMap(int y,int x){
 	return x>=0 && x<m && y>=0 &&y<n;
 }
 
-int getValue(int y, int x,int k){
-	int tmp,flg,cnt=0;
-	for(int d=0;d<4;++d){
-		tmp=0;
-		flg=1;
-		for(int i=0;i<4;++i){
-			for(int j=0;j<4;++j){
-				if(block[k][d][i][j]){
-					if(!isOnMap(y+i,x+j)){
-					flg=0;
-					}
-				}
-			}
-		}
-		if(!flg) continue;
-		for(int i=0;i<4;++i){
-			for(int j=0;j<4;++j){
-				int ny=y+i;
-				int nx=x+j;
-				if(block[k][d][i][j]){
-					tmp+=map[ny][nx];
-				}
-			}
-		}
-		cnt=_max(cnt,tmp);
+void dfs(int y, int x,int cnt,int val){
+	if(cnt==4){
+		ans=_max(ans,val);
+		return;
 	}
-	return cnt;
+	
+	for(int d=0;d<4;++d){
+		int nx=dx[d]+x;
+		int ny=dy[d]+y;
+		if(!isOnMap(ny,nx)) continue;
+		if(visit[ny][nx]) continue;
+		visit[ny][nx]=1;
+		dfs(ny,nx,cnt+1,val+map[ny][nx]);
+		visit[ny][nx]=0;
+	}
 }
 
-int solve(){
-	int ret=0;
-	for(int i=0;i<n;++i){
-		for(int j=0;j<m;++j){
-			for(int k=0;k<7;++k){
-				ret=_max(ret,getValue(i,j,k));
+void solve(int y, int x){
+	for(int d=0;d<4;++d){
+		int possi=1;
+		int cnt=map[y][x];
+		for(int d2=0;d2<4;++d2){
+			if(d==d2) continue;
+			int nx=dx[d2]+x;
+			int ny=dy[d2]+y;
+			if(!isOnMap(ny,nx)) possi=0;
+		}
+		if(possi){
+			for(int d2=0;d2<4;++d2){
+				if(d==d2) continue;
+				int nx=dx[d2]+x;
+				int ny=dy[d2]+y;
+				cnt+=map[ny][nx];
 			}
+			ans=_max(ans,cnt);
 		}
 	}
-	return ret;
 }
 
 int main(void){
@@ -190,6 +60,15 @@ int main(void){
 		}
 	}
 	
-	cout<<solve();
+	for(int i=0;i<n;++i){
+		for(int j=0;j<m;++j){
+			visit[i][j]=1;
+			dfs(i,j,1,map[i][j]);
+			visit[i][j]=0;
+			solve(i,j);
+		}
+	}
+
+	cout<<ans;
 	return 0;
 }
