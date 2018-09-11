52ff9045a003f30467214dd9bcd79e25d226fb78
skrudtn <skrudtn703@naver.com>
2018-09-11 16:17:26 +0900

    Solve again

--- acmicpc/13458_시험감독.cpp
+++ acmicpc/13458_시험감독.cpp
@@ -1,29 +1,26 @@
-#include <iostream>
+#include <stdio.h>
 
-using namespace std;
-
-int n,b,c,room[1000001];
-
-long long solve(){
-	long long cnt=0;
-	// n=1000000, room[i]=1000000, b=c=1 일때 cnt가 int 범위 벗어남. 
-	for(int i=1;i<=n;++i){
-		room[i]-=b;
-		cnt++;
-		if(room[i]>0){
-			cnt+=room[i]/c;
-			if(room[i]%c) cnt++;
-		}
-	}
-	return cnt;
-}
+int n,b,c,arr[1000001];
 
 int main(void){
-	cin>>n;
+	long long ans;
+	scanf("%d",&n);
 	for(int i=1;i<=n;++i){
-		cin>>room[i];
+		scanf("%d",&arr[i]);
 	}
-	cin>>b>>c;
-	cout<<solve();
+	scanf("%d %d",&b,&c);
+	ans=n;
+	for(int i=1;i<=n;++i){
+		arr[i]-=b;
+		if(arr[i]<0) arr[i]=0;
+		if(arr[i] % c == 0) {
+			ans+= arr[i]/c;
+		}
+		else {
+			ans+=arr[i]/c+1;
+		}
+	}
+	
+	printf("%lld",ans);
 	return 0;
 }
